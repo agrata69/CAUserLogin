aa13a5ad8c73d457f51f41e041052c0d20ad51ea
Agrata Gupta <agrata.gupta@mail.utoronto.ca>
2024-10-23 16:25:01 -0400

    Task 2 finished

--- src/main/java/data_access/DBUserDataAccessObject.java
+++ src/main/java/data_access/DBUserDataAccessObject.java
@@ -20,8 +20,8 @@ import use_case.signup.SignupUserDataAccessInterface;
  * The DAO for user data.
  */
 public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
-                                               LoginUserDataAccessInterface,
-                                               ChangePasswordUserDataAccessInterface {
+        LoginUserDataAccessInterface,
+        ChangePasswordUserDataAccessInterface {
     private static final int SUCCESS_CODE = 200;
     private static final String CONTENT_TYPE_LABEL = "Content-Type";
     private static final String CONTENT_TYPE_JSON = "application/json";
@@ -63,6 +63,11 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
         catch (IOException | JSONException ex) {
             throw new RuntimeException(ex);
         }
+    }
+
+    @Override
+    public void setCurrentUser(String name) {
+
     }
 
     @Override
